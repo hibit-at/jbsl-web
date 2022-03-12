bf62458a61cc58475abb76387161e27a2f59c02c
unknown <hibit_at@hotmail.com>
2022-03-12 19:50:27 +0900

    seaech JP

--- league_update.py
+++ league_update.py
@@ -16,34 +16,35 @@ def league_update_process():
             print(player)
             for song in league.playlist.songs.all():
                 print(song, song.lid)
-                url = f'https://scoresaber.com/api/leaderboard/by-id/{song.lid}/scores?search={player.name}'
+                url = f'https://scoresaber.com/api/leaderboard/by-id/{song.lid}/scores?countries=JP&search={player.name}'
                 res = requests.get(url).json()
                 if 'errorMessage' in res:
                     print('no score')
                     continue
                 print(res)
                 notes = song.notes
-                scoreData = res['scores'][0]
-                hitname = scoreData['leaderboardPlayerInfo']['name']
-                if player.name != hitname:
-                    print('wrong name!')
-                    continue
-                score = scoreData['modifiedScore']
-                rawPP = scoreData['pp']
-                miss = scoreData['badCuts'] + scoreData['missedNotes']
-                defaults = {
-                    'score': score,
-                    'acc': score/(115*8*int(notes)-7245)*100,
-                    'rawPP': rawPP,
-                    'miss': miss,
-                }
-                Score.objects.update_or_create(
-                    player=player,
-                    song=song,
-                    league=league,
-                    defaults=defaults,
-                )
-                print('score updated!')
+                for scoreData in res['scores']:
+                    hitname = scoreData['leaderboardPlayerInfo']['name']
+                    if player.name != hitname:
+                        print('wrong name!')
+                        continue
+                    score = scoreData['modifiedScore']
+                    rawPP = scoreData['pp']
+                    miss = scoreData['badCuts'] + scoreData['missedNotes']
+                    defaults = {
+                        'score': score,
+                        'acc': score/(115*8*int(notes)-7245)*100,
+                        'rawPP': rawPP,
+                        'miss': miss,
+                    }
+                    Score.objects.update_or_create(
+                        player=player,
+                        song=song,
+                        league=league,
+                        defaults=defaults,
+                    )
+                    print('score updated!')
+                    break
         print(league)
         players, songs = calculate_scoredrank_LBs(league)
         for i, player in enumerate(players[:3]):

8efa1a8d904f4295a970c57e08acbaa5982cc542
unknown <hibit_at@hotmail.com>
2022-03-13 04:51:07 +0900

    ditinct

--- app/views.py
+++ app/views.py
@@ -536,8 +536,7 @@ def calculate_scoredrank_LBs(league, virtual=None):
     from django.db.models import Q
     for song in songs:
         query = Score.objects.filter(
-            song=song, league=league).filter(Q(player__league=league) | Q(player=virtual)).order_by('-score')
-        print(query)
+            song=song, league=league).filter(Q(player__league=league) | Q(player=virtual)).order_by('-score').distinct()
         for rank, score in enumerate(query):
             pos = base + slope(rank + 1)
             setattr(score, 'rank', rank+1)

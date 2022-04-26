224f813521245dcbcd86085ce88334fb7d0e3aa9
unknown <hibit_at@hotmail.com>
2022-04-27 00:24:27 +0900

    search field

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from app.models import Player, Playlist, Song, League, Score, LeagueComment, Headline, SongInfo
 
+class ScoreQuestion(admin.ModelAdmin):
+    search_fields = ['player__name','song__title']
+
 # Register your models here.
 
 admin.site.register(Player)
 admin.site.register(Song)
 admin.site.register(League)
-admin.site.register(Score)
+admin.site.register(Score, ScoreQuestion)
 admin.site.register(Playlist)
 admin.site.register(LeagueComment)
 admin.site.register(Headline)

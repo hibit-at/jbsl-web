0286a77014fc7d0fde37db4d539c0041b16bf53e
unknown <hibit_at@hotmail.com>
2022-11-07 12:39:17 +0900

    migrate ownercomment

--- app/models.py
+++ app/models.py
@@ -96,6 +96,7 @@ class League(models.Model):
     third = models.ForeignKey(
         Player, on_delete=models.SET_NULL, null=True, blank=True, related_name='third')
     isOfficial = models.BooleanField(default=False)
+    ownerComment = models.CharField(default='', max_length=1000)
 
     def __str__(self):
         return str(self.name)

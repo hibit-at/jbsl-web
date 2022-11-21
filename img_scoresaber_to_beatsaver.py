f09e262c39a5af5f9a9e6fd6e97baa9fd0da05a0
unknown <hibit_at@hotmail.com>
2022-11-21 23:51:47 +0900

    img scoresaber to beatsaver

--- img_scoresaber_to_beatsaver.py
+++ img_scoresaber_to_beatsaver.py
@@ -2,14 +2,17 @@ import os
 import django
 import sys
 
-def process():
+def process(pk):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbsl3.settings')
     django.setup()
-    from app.models import Song
-    for song in Song.objects.all():
+    from app.models import Song, Playlist
+    playlist = Playlist.objects.get(pk=pk)
+    for song in playlist.songs.all():
         url = f'https://eu.cdn.beatsaver.com/{song.hash.lower()}.jpg'
         song.imageURL = url
         song.save()        
 
 if __name__ == '__main__':
-    process()
+    args = sys.argv
+    pk = int(args[1])
+    process(pk)

fc00c44bd97c944b0802aed6532434b58424f629
unknown <hibit_at@hotmail.com>
2023-04-23 08:43:27 +0900

    badge select order

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from app.models import Player, Playlist, Song, League, Score, Participant, Headline, SongInfo, Badge, JPMap, Match, DGA
 # Register your models here.
 
+class BadgeAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "player":
+            kwargs["queryset"] = Player.objects.order_by('name')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Player, search_fields=['name'])
 admin.site.register(Song, search_fields=['title', 'author'])
@@ -11,7 +17,7 @@ admin.site.register(Playlist, search_fields=['title', 'editor'])
 admin.site.register(Participant, search_fields=['player__name', 'league__name'])
 admin.site.register(Headline, search_fields=['player__name'], readonly_fields=['score','player'])
 admin.site.register(SongInfo)
-admin.site.register(Badge)
+admin.site.register(Badge, BadgeAdmin)
 admin.site.register(JPMap, search_fields=['name', 'uploader__name'])
 admin.site.register(Match)
 admin.site.register(DGA)

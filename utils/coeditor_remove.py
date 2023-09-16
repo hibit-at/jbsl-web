220fb16a02a72bdcb65b78cb34e893e6b0ad4761
unknown <hibit_at@hotmail.com>
2023-09-16 12:48:42 +0900

    coeditor remove

---
+++ utils/coeditor_remove.py
@@ -0,0 +1,28 @@
+import os
+import django
+
+def process():
+    import sys
+    cwd = os.getcwd()
+    sys.path.append(cwd)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbsl3.settings')
+    django.setup()
+    from app.models import Playlist, Player
+    playlist_ids_str = input('input playlist ids\n')
+    playlist_ids = [int(pid) for pid in playlist_ids_str.split(',')]
+    matching_playlists = Playlist.objects.filter(pk__in=playlist_ids)
+    for playlist in matching_playlists:
+        print(playlist)
+    player_sids_str = input('input player sids\n')
+    player_sids = [sid.strip() for sid in player_sids_str.split(',')]
+    matching_players = Player.objects.filter(sid__in=player_sids)
+    for player in matching_players:
+        print(player)
+    input('okay?')
+    for playlist in matching_playlists:
+        for player in matching_players:
+            playlist.CoEditor.remove(player)
+
+
+if __name__ == '__main__':
+    process()
